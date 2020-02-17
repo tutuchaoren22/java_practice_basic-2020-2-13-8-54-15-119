e8fffcd7f2fe85ae93360742da8f469a74c96696
duyuqian <15738381799@163.com>
2020-02-17 12:29:00 +0800

    [duyuqian] feat: create the ParseOrder class to analysis the orderInformation object

---
+++ src/main/java/com/thoughtworks/ParseOrder.java
@@ -0,0 +1,50 @@
+package com.thoughtworks;
+
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ParseOrder {
+    private Map<Dish, Integer> parseOrder;
+    private Double totalPrice;
+
+    public ParseOrder() {
+    }
+
+    public ParseOrder(OrderInformation orderInformation) {
+        this.parseOrder = parseOrderToMap(orderInformation.getOrder());
+        this.totalPrice = calculationTotalPrice();
+    }
+
+    private Map<Dish, Integer> parseOrderToMap(String order) {//"ITEM0001 x 1,ITEM0013 x 2,ITEM0022 x 1"
+        Map<Dish, Integer> parseOrder = new LinkedHashMap();
+        List<Dish> dishes = DataProvider.getDishes();
+        for (String orderPer : order.split(",")) {
+            String[] dishIdAndCount = orderPer.split(" x ");
+            for (Dish dish : dishes) {
+                if (dishIdAndCount[0].equals(dish.getId())) {
+                    parseOrder.put(dish, Integer.valueOf(dishIdAndCount[1]));
+                    break;
+                }
+            }
+        }
+        return parseOrder;
+    }
+
+    private Double calculationTotalPrice() {
+        double totalPrice = 0.0;
+        for (Dish key : this.parseOrder.keySet()) {
+            totalPrice += key.getPrice() * this.parseOrder.get(key);
+        }
+        return totalPrice;
+    }
+
+    public Map<Dish, Integer> getParseOrder() {
+        return parseOrder;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+}
