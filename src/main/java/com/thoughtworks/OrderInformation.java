8a99fcc9ed1f50be254be7aa9c83fd9b66648df6
duyuqian <15738381799@163.com>
2020-02-17 12:26:03 +0800

    [duyuqian] feat: create the OrderInformation class having the order input by user

---
+++ src/main/java/com/thoughtworks/OrderInformation.java
@@ -0,0 +1,17 @@
+package com.thoughtworks;
+
+public class OrderInformation {
+    private String order;
+
+    public OrderInformation(String order) {
+        this.order = order;
+    }
+
+    public OrderInformation() {
+
+    }
+
+    public String getOrder() {
+        return order;
+    }
+}
