3f8605b2057205b7620a02160aaa52cce3ae726e
duyuqian <15738381799@163.com>
2020-02-17 12:46:22 +0800

    [duyuqian] fix: calculate the bestCharge for Restaurant

--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -2,7 +2,11 @@ package com.thoughtworks;
 
 public class Restaurant {
 
-  public String bestCharge(String selectedItems) {
-    return null;
-  }
+    public String bestCharge(String selectedItems) {
+        OrderInformation orderInformation = new OrderInformation(selectedItems);
+        ParseOrder parseOrder = new ParseOrder(orderInformation);
+        PromotionResult promotionResult = new PromotionResult(parseOrder);
+        Summary summary = new Summary(parseOrder, promotionResult.getPromotionResult());
+        return summary.getSummary();
+    }
 }
