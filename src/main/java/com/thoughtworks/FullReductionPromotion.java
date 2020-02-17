e7c34508f80d8b9699b953cab8b2f084f085c835
duyuqian <15738381799@163.com>
2020-02-17 12:35:54 +0800

    [duyuqian] feat: create FullReductionPromotion class implements the Promotions

---
+++ src/main/java/com/thoughtworks/FullReductionPromotion.java
@@ -0,0 +1,24 @@
+package com.thoughtworks;
+
+public class FullReductionPromotion implements Promotions {
+    Double reducedMoney;
+
+    @Override
+    public Double getReducedMoney() {
+        return reducedMoney;
+    }
+
+    @Override
+    public void setReducedMoney(ParseOrder parseOrder) {
+        if (parseOrder.getTotalPrice() >= 30) {
+            this.reducedMoney = 6.0;
+        } else {
+            this.reducedMoney = 0.0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "-----------------------------------\n" + "使用优惠:\n" + "满30减6元，省6元\n";
+    }
+}
