223679ba2957532e75374841609759fc70c5d3cf
duyuqian <15738381799@163.com>
2020-02-17 12:40:41 +0800

    [duyuqian] feat: create PromotionResult class having choosePromotion method

---
+++ src/main/java/com/thoughtworks/PromotionResult.java
@@ -0,0 +1,43 @@
+package com.thoughtworks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromotionResult {
+    private Promotions promotionResult;
+
+    public PromotionResult() {
+    }
+
+    public PromotionResult(ParseOrder parseOrder) {
+        this.promotionResult = choosePromotion(parseOrder);
+    }
+
+    private Promotions choosePromotion(ParseOrder parseOrder) {
+        Promotions fullReductionPromotion = new FullReductionPromotion();
+        fullReductionPromotion.setReducedMoney(parseOrder);
+
+        Promotions halfPricePromotion = new HalfPricePromotion();
+        halfPricePromotion.setReducedMoney(parseOrder);
+
+        Promotions noPromotion = new NoPromotion();
+        noPromotion.setReducedMoney(parseOrder);
+
+        List<Promotions> promotionsList = new ArrayList<>();
+        promotionsList.add(noPromotion);
+        promotionsList.add(fullReductionPromotion);
+        promotionsList.add(halfPricePromotion);
+
+        Promotions promotionResult = noPromotion;
+        for (Promotions promotions : promotionsList) {
+            if (promotions.getReducedMoney() > promotionResult.getReducedMoney()) {
+                promotionResult = promotions;
+            }
+        }
+        return promotionResult;
+    }
+
+    public Promotions getPromotionResult() {
+        return promotionResult;
+    }
+}
